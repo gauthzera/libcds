cfd8d1ecff3566a525af6817a6231509af68fd9c
Gabriel Ulhoa <gabrielulhoa@outlook.com>
2026-08-21 11:59:27 -0300

    add: include libcds.h

---
+++ include/libcds.h
@@ -0,0 +1,13 @@
+#ifndef LIBCDS_H
+#define LIBCDS_H
+
+#include "cds_vector.h"
+#include "cds_list.h"
+#include "cds_queue.h"
+#include "cds_stack.h"
+#include "cds_bst.h"
+#include "cds_hash.h"
+#include "cds_avl.h"
+#include "cds_trie.h"
+
+#endif
